7a2974d8675d9e8d8b1b0f151f5c64f81fd6f05a
Antony Warner <awarner@gresham-computing.com>
2015-07-22 23:19:52 +0100

    Basic project structure

---
+++ src/main/java/net/antonywarner/guava/eventbus/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides convenience annotations for declaring Guava EventBus publishers and subscribers, and for indicating that a method should post to an EventBus.
+ *
+ * @author Antony Warner
+ */
+package net.antonywarner.guava.eventbus.annotations;
---
+++ src/main/java/net/antonywarner/guava/eventbus/annotations/spring/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides basic Spring integration for Guava EventBus for registering subscribers with an EventBus, and to support posting of method results to an EventBus using @Publish
+ *
+ * @author Antony Warner
+ */
+package net.antonywarner.guava.eventbus.annotations.spring;
